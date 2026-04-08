ebd9c1f93bfc15312bc1038673da3804e176255f
Juan Collante Dominguez <85765829+juancollante@users.noreply.github.com>
2026-04-08 17:57:09 -0500

    Delete logistica-envios directory

--- logistica-envios/src/main/java/com/logistica/logistica_envios/domain/port/in/GetShipmentByTrackingUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.logistica.logistica_envios.domain.port.in;
-
-import com.logistica.logistica_envios.domain.model.ShipmentDetail;
-
-public interface GetShipmentByTrackingUseCase {
-
-    ShipmentDetail getByNumeroGuia(String numeroGuia);
-}
--- logistica-envios/src/main/resources/db/migration/V2__seed_estados_envio.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO estados_envio (id, codigo, nombre, descripcion) VALUES
-    ('a0000001-0000-4000-8000-000000000001', 'CREADO', 'Creado', 'Radicado en el sistema'),
-    ('a0000001-0000-4000-8000-000000000002', 'EN_TRANSITO', 'En tránsito', 'En movimiento entre nodos'),
-    ('a0000001-0000-4000-8000-000000000003', 'EN_SEDE', 'En sede', 'En oficina o hub'),
-    ('a0000001-0000-4000-8000-000000000004', 'ENTREGADO', 'Entregado', 'Entrega confirmada'),
-    ('a0000001-0000-4000-8000-000000000005', 'INCIDENCIA', 'Incidencia', 'Requiere atención');
